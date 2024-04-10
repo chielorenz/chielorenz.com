24c135ea577f191a346b61c7749cc58e2e632e86
Luca Lorenzini <chielorenz@gmail.com>
2024-04-10 23:28:09 +0200

    Show only published posts

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -23,15 +23,17 @@ export async function GET() {
 		// Webpack need come information about static import path see https://webpack.js.org/api/module-methods/#dynamic-expressions-in-import
 		const post = await import(`@/posts/${file.name}`);
 
-		// Generate a small version of the cover to be used as image placeholder, see https://nextjs.org/docs/app/api-reference/components/image#blurdataurl
-		const base64 = await sharp(process.cwd() + "/public/" + post.meta.cover)
-			.resize({ width: 10 })
-			.toFormat("webp")
-			.toBuffer()
-			.then((buffer: Buffer) => buffer.toString("base64"));
-		const placeholder = `data:image/webp;base64,${base64}`;
+		if (post.meta.published) {
+			// Generate a small version of the cover to be used as image placeholder, see https://nextjs.org/docs/app/api-reference/components/image#blurdataurl
+			const base64 = await sharp(process.cwd() + "/public/" + post.meta.cover)
+				.resize({ width: 10 })
+				.toFormat("webp")
+				.toBuffer()
+				.then((buffer: Buffer) => buffer.toString("base64"));
+			const placeholder = `data:image/webp;base64,${base64}`;
 
-		postsMeta.push({ ...post.meta, fileName, slug, href, placeholder });
+			postsMeta.push({ ...post.meta, fileName, slug, href, placeholder });
+		}
 	}
 
 	postsMeta.sort((a, b) => Date.parse(b.timestamp) - Date.parse(a.timestamp));
