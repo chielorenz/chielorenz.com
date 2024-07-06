e13adb601bad811d19694212c522acbc316f2a6b
Luca Lorenzini <chielorenz@gmail.com>
2024-07-06 17:24:49 +0200

    "module" is reserved

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -18,12 +18,12 @@ export async function GET() {
 	for (const file of files) {
 		const slug = path.parse(file.name).name;
 		const href = path.join("blog", slug);
-		const module = file.fullpath().replace(baseDir, "");
+		const modulePath = file.fullpath().replace(baseDir, "");
 		// Webpack need come information about static import path see https://webpack.js.org/api/module-methods/#dynamic-expressions-in-import
-		const post = await import(`@/posts/${module}`);
+		const post = await import(`@/posts/${modulePath}`);
 
 		if (post.meta.published) {
-			postsMeta.push({ ...post.meta, href, slug, module });
+			postsMeta.push({ ...post.meta, href, slug, module: modulePath });
 		}
 	}
 
